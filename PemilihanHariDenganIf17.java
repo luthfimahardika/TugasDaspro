eefe153e9591cfd4933589e05970d418fb6a8f4e
luthfimahardika <lutfi.mahardika17@gmail.com>
2023-10-04 15:57:23 +0700

    boys

---
+++ PemilihanHariDenganIf17.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class PemilihanHariDenganIf17 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan angka (1-7): ");
+        int angka = sc.nextInt();
+
+        if (angka >= 1 && angka <= 5) {
+            System.out.println("Weekday");
+        } else if (angka == 6 || angka == 7) {
+            System.out.println("Weekend");
+        } else {
+            System.out.println("Invalid Number");
+        }
+
+        sc.close();
+      }
+}
