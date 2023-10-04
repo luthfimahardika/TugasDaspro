c324e01bd65a462f359036f35ccf07ef9d0c9c1b
luthfimahardika <lutfi.mahardika17@gmail.com>
2023-10-04 13:25:14 +0700

    wow

---
+++ PemilihanSwitch17.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class PemilihanSwitch17 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        double angka1, angka2, hasil = 0;
+        char operator;
+
+        System.out.print("Masukkan angka1: ");
+        angka1 = sc.nextDouble();
+        System.out.print("Masukkan angka2: ");
+        angka2 = sc.nextDouble();
+        System.out.print("Masukkan operator (+ - * :)");
+        operator = sc.next().charAt(0);
+
+    switch (operator) {
+        case '+':
+            hasil = angka1 + angka2;
+            break;
+        case '-':
+            hasil = angka1 - angka2;
+            break;
+        case '/':
+            hasil = angka1 / angka2;
+            break;
+    }
+
+    System.out.println(angka1 + " " +  operator + " " + angka2 + " = " + hasil);
+
+    }
+}
