c2e3d3bfcf7f32be64e7a0f54b8da7083b963d14
luthfimahardika <lutfi.mahardika17@gmail.com>
2023-10-11 14:41:07 +0700

    alhamdulillah

--- Segitiga17.java
+++ Segitiga17.java
@@ -18,8 +18,12 @@ public class Segitiga17 {
         if (totalsudut == 180) {
             if ((sudut1 == 90) || (sudut2 == 90) || (sudut3 == 90))
                 System.out.println("Segitiga tersebut adalah segitiga siku-siku");
-            else 
-                System.out.println("Segitiga tersebut adalah bukan segitiga siku-siku");
+            else if((sudut1 == 60) && (sudut2 == 60) && (sudut3 == 60)) 
+                System.out.println("Segitiga tersebut adalah segitiga sama sisi");
+            else if ((sudut1 == sudut2 ) || (sudut3 == sudut1) || (sudut2 == sudut3))
+                System.out.println("Segitiga tersebut adalah segitiga sama kaki");
+            else
+                System.out.println("segitiga tersebut segitiga sembarang");
         } else
             System.out.println("Bukan segitiga");
 
