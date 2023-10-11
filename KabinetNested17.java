ab1b0aa855cf69378c2bee8d13afb979485c40cd
luthfimahardika <lutfi.mahardika17@gmail.com>
2023-10-11 11:20:31 +0700

    holaa

---
+++ KabinetNested17.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class KabinetNested17{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Masukkan Tahun");
+        int tahun = sc.nextInt();
+
+        if ((tahun % 4) == 0) {
+            if ((tahun % 100) != 0)
+            System.out.println("Tahun Kabisat");
+        } else
+            System.out.println("Bukan Tahun Kabisat");
+
+        }
+    }
