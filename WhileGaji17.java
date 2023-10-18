8d4228c148859da2821de9cfd69d42a5dde282e2
luthfimahardika <lutfi.mahardika17@gmail.com>
2023-10-18 15:07:42 +0700

    onok

--- WhileGaji17.java
+++ WhileGaji17.java
@@ -27,8 +27,12 @@ public class WhileGaji17 {
             }
             else if (jabatan.equalsIgnoreCase("karyawan")) {
                 gajiLembur = jumlahJamLembur *75000;
+            } else {
+                System.out.println("Jabatan invalid");
+                i--;
+                continue;
             }
-
+            
             totalGajiLembur += gajiLembur;
         }
         System.out.println("Total gaji lembur: " + totalGajiLembur);
