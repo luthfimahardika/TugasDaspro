075ea08c77ba3f4d71254d0ca88a87df9261cce4
luthfimahardika <lutfi.mahardika17@gmail.com>
2023-10-18 15:52:28 +0700

    pertama

--- KabinetNested17.java
+++ KabinetNested17.java
@@ -10,6 +10,8 @@ public class KabinetNested17{
         if ((tahun % 4) == 0) {
             if ((tahun % 100) != 0)
             System.out.println("Tahun Kabisat");
+                if ((tahun % 400) == 0)
+                System.out.println("Tahun Kabisat");
             else
             System.out.println("Bukan Tahun Kabisat");
         } 
