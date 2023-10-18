1718624bf0bd6bf2eade45d1991b8c7f62a5cd62
luthfimahardika <lutfi.mahardika17@gmail.com>
2023-10-18 11:16:33 +0700

    assalamualaikum

---
+++ ForKelipatan17.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class ForKelipatan17 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int bilangan, total=0, counter=0;
+
+        System.out.println("Masukkan bilangan 1-9: ");
+        bilangan = sc.nextInt();
+
+        for (int i = 0; i < 50; i++) {
+            if (i % bilangan == 0) {
+                total += i;
+                counter++;
+            }
+        }
+        System.out.printf("Pada rentang 1-50 terdapat %d bilangan kelipatan %d dengan total %d", counter, bilangan, total);
+
+    }
+}
