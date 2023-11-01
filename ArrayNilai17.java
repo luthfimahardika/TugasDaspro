5d3010af2a2a58f4cb20170fd04ffc4662049265
luthfimahardika <lutfi.mahardika17@gmail.com>
2023-11-01 14:16:55 +0700

    song

--- ArrayNilai17.java
+++ ArrayNilai17.java
@@ -5,17 +5,38 @@ public class ArrayNilai17 {
         Scanner sc = new Scanner(System.in);
 
         int[] nilaiAkhir = new int[10];
-        
+        int totalNilai = 0;
+        int nilaiTertinggi = 0;
+        int nilaiTerendah = 100;
+        int mahasiswaLulus = 0;
+        int mahasiswaTidakLulus = 0;
+
         for (int i = 0; i < nilaiAkhir.length; i++) {
-            System.out.println("Masukkan nilai akhir mahasiswa ke-" + (i+1) + ": ");
+            System.out.print("Masukkan nilai akhir mahasiswa ke-" + (i + 1) + " : ");
             nilaiAkhir[i] = sc.nextInt();
-        }
-        for (int i = 0; i < 10; i++) {
+            totalNilai += nilaiAkhir[i];
+
+            if (nilaiAkhir[i] > nilaiTertinggi) {
+                nilaiTertinggi = nilaiAkhir[i];
+            }
+            if (nilaiAkhir[i] < nilaiTerendah) {
+                nilaiTerendah = nilaiAkhir[i];
+            }
             if (nilaiAkhir[i] > 70) {
-            System.out.println("Mahasiswa ke-" + (i+1) + " lulus ");
-            }else {
-                System.out.println("Mahasiswa ke-" + " tidak lulus ");
+                mahasiswaLulus++;
+            } else {
+                mahasiswaTidakLulus++;
             }
         }
+
+        double rataRata = (double) totalNilai / nilaiAkhir.length;
+
+        System.out.println("Nilai rata-rata: " + rataRata);
+        System.out.println("Nilai tertinggi: " + nilaiTertinggi);
+        System.out.println("Nilai terendah: " + nilaiTerendah);
+        System.out.println("Jumlah mahasiswa yang lulus: " + mahasiswaLulus);
+        System.out.println("Jumlah mahasiswa yang tidak lulus: " + mahasiswaTidakLulus);
+
+        sc.close();
     }
 }
