daa5569f42b66155ed9c0df9176b38f02ea8bab6
luthfimahardika <lutfi.mahardika17@gmail.com>
2023-11-08 00:09:13 +0700

    tes

---
+++ Praktikum03/ContohTipeData17.java
@@ -0,0 +1,21 @@
+public class ContohTipeData17{
+    public static void main(String[] args){
+
+        char golonganDarah = 'A';
+        short jumlahPenduduk = 1025;
+        float suhu = 60.50f;
+        double berat= 0.5467812345;
+        long saldo = 150000000;
+        int angkaDesimal = 0x10;
+        
+        System.out.println("Golongan darah\t: " + (byte)golonganDarah);
+        System.out.println("Jarak\t\t: " + jumlahPenduduk);
+        System.out.println("Suhu\t\t: " + suhu);
+        System.out.println("Berat\t\t: " + (float)berat);
+        System.out.println("Saldo\t\t: " + saldo);
+        System.out.println("Angka desimal\t: " + angkaDesimal);
+
+
+    
+    }
+}
---
+++ Praktikum03/ContohVariabel17.java
@@ -0,0 +1,16 @@
+public class ContohVariabel17{
+    public static void main(String[] args){
+        String namaBapakSayaAdalah = "David Beckham";
+        boolean isPandai = true;
+        char jenisKelamin = 'L';
+        byte _umurSayaSekarang = 19;
+        double $ipk = 3.24, tinggi = 1.78;
+        
+        System.out.println(namaBapakSayaAdalah);
+        System.out.println("Apakah pandai?" + isPandai);
+        System.out.println("jenis kelamin" + jenisKelamin);
+        System.out.println("IPK: " + $ipk);
+        System.out.println(String.format("Saya berumur %s dengan tinggi badan %s", _umurSayaSekarang, tinggi));
+        
+    }
+}
