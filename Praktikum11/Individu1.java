ee704eacfcd31ee79618eabfdaabf82437679d81
luthfimahardika <lutfi.mahardika17@gmail.com>
2023-11-15 15:55:18 +0700

    tes

--- Praktikum11/Individu1.java
+++ Praktikum11/Individu1.java
@@ -7,15 +7,21 @@ public class Individu1 {
         System.out.println("Masukkan angka: ");
         int n = sc.nextInt();
 
-        for (int i = 0; i <= n; i++) {
-            for (int j = 0; j < n - i; j++) {
+        if (n>2) {
+            for (int i = 0; i <= n; i++) {
+                for (int j = 0; j < n - i; j++) {
                 System.out.print(" ");
-            }
-            for (int j = 0; j < i; j++) {
-                int angka = j + 1;
-                System.out.print(angka);
-            }
+                }
+
+                for (int j = 0; j < i; j++) {
+                    int angka = j + 1;
+                    System.out.print(angka);
+                }   
             System.out.println();
+            }
+        } else {
+            System.out.println("Angka valid");
         }
+    
     }
 }
