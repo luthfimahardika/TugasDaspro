a9adccae38179570dfb18b5ea1e129d536807dc2
luthfimahardika <lutfi.mahardika17@gmail.com>
2023-11-29 13:06:26 +0700

    tes

--- Praktikum13/Welcome17.java
+++ Praktikum13/Welcome17.java
@@ -2,9 +2,7 @@ package Praktikum13;
 
 public class Welcome17 {
     public static void main(String[] args) {
-        displayWelcomeMessage();
-    }
-    public static void displayWelcomeMessage(){
         System.out.println("Selamat datang di Politeknik Negeri Malang");
+    
     }
 }
---
+++ Praktikum13/Welcome217.java
@@ -0,0 +1,13 @@
+package Praktikum13;
+
+public class Welcome217 {
+    public static void main(String[] args) {
+        displayWelcomeMessage("Ani");
+        displayWelcomeMessage("Budi");
+        displayWelcomeMessage("Cica");
+        displayWelcomeMessage("Dodi");
+    }
+    public static void displayWelcomeMessage(String name){
+        System.out.println("Selamat datang di Politeknik Negeri Malang, " + name);
+    }
+}
