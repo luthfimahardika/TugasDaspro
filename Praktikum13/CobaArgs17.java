6d9ee2922104fd2d05582a54a84267c24cc7020a
luthfimahardika <lutfi.mahardika17@gmail.com>
2023-11-29 14:58:10 +0700

    tes

---
+++ Praktikum13/CobaArgs17.java
@@ -0,0 +1,14 @@
+package Praktikum13;
+
+public class CobaArgs17 {
+    public static void main(String[] args) {
+
+        if (1 < args.length) {
+            System.out.println("Hai " + args[0]);
+            System.out.println("Selamat datang di " + args[1]);
+        } else {
+            System.out.println("Hai, Selamat datang ");
+        }
+    
+    }
+}
