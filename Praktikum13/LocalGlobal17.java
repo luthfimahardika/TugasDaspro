578ef8a364ecbc3a27a3b18f9d859a9bb5481f42
luthfimahardika <lutfi.mahardika17@gmail.com>
2023-12-06 15:43:49 +0700

    tes

--- Praktikum13/LocalGlobal17.java
+++ Praktikum13/LocalGlobal17.java
@@ -1,10 +1,9 @@
-package Praktikum13;
-
 public class LocalGlobal17 {
     static int a = 10;
     public static void main(String[] args) {
         int b = 10;
         int c = 17;
+        a = a + 8;
 
         System.out.println("Nilai a: " + a);
         System.out.println("Nilai b: " + b);
@@ -16,6 +15,7 @@ public class LocalGlobal17 {
     private static void display() {
         int b = 20;
         int c = 5;
+        a = a + 12;
 
         System.out.println("Nilai a: " + a);
         System.out.println("Nilai b: " + b);
